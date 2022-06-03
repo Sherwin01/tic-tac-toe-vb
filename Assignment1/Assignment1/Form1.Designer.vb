f46877732077b3db74365f2e06890876fc599ccd
lim <sherwinlim8899@gmail.com>
2022-06-04 05:27:15 +0800

    tictactoe

---
+++ Assignment1/Assignment1/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(189, 82)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(100, 100)
+        Me.btn1.TabIndex = 0
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(295, 82)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(100, 100)
+        Me.btn2.TabIndex = 1
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(401, 82)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(100, 100)
+        Me.btn3.TabIndex = 2
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(189, 188)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(100, 100)
+        Me.btn4.TabIndex = 3
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(295, 188)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(100, 100)
+        Me.btn5.TabIndex = 4
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(401, 188)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(100, 100)
+        Me.btn6.TabIndex = 5
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(189, 294)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(100, 100)
+        Me.btn7.TabIndex = 6
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(295, 294)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(100, 100)
+        Me.btn8.TabIndex = 7
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(401, 294)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(100, 100)
+        Me.btn9.TabIndex = 8
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+End Class
---
+++ Assignment1/Assignment1/Form1.vb
@@ -0,0 +1,144 @@
+﻿Public Class Form1
+    Dim XorO As Boolean = False
+    Sub ButtonsEnableFalse()
+        btn1.Enabled = False
+        btn2.Enabled = False
+        btn3.Enabled = False
+        btn4.Enabled = False
+        btn5.Enabled = False
+        btn6.Enabled = False
+        btn7.Enabled = False
+        btn8.Enabled = False
+        btn9.Enabled = False
+    End Sub
+
+    Sub check()
+        If btn1.Text = "X" And btn2.Text = "X" And btn3.Text = "X" Then
+            btn1.BackColor = Color.Blue
+            btn2.BackColor = Color.Blue
+            btn3.BackColor = Color.Blue
+            MessageBox.Show("X is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn4.Text = "X" And btn5.Text = "X" And btn6.Text = "X" Then
+            btn4.BackColor = Color.Blue
+            btn5.BackColor = Color.Blue
+            btn6.BackColor = Color.Blue
+            MessageBox.Show("X is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn7.Text = "X" And btn8.Text = "X" And btn9.Text = "X" Then
+            btn7.BackColor = Color.Blue
+            btn8.BackColor = Color.Blue
+            btn9.BackColor = Color.Blue
+            MessageBox.Show("X is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn1.Text = "X" And btn4.Text = "X" And btn7.Text = "X" Then
+            btn1.BackColor = Color.Blue
+            btn4.BackColor = Color.Blue
+            btn7.BackColor = Color.Blue
+            MessageBox.Show("X is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn1.Text = "X" And btn5.Text = "X" And btn9.Text = "X" Then
+            btn1.BackColor = Color.Blue
+            btn5.BackColor = Color.Blue
+            btn9.BackColor = Color.Blue
+            MessageBox.Show("X is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn3.Text = "X" And btn6.Text = "X" And btn9.Text = "X" Then
+            btn3.BackColor = Color.Blue
+            btn6.BackColor = Color.Blue
+            btn9.BackColor = Color.Blue
+            MessageBox.Show("X is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn3.Text = "X" And btn5.Text = "X" And btn7.Text = "X" Then
+            btn3.BackColor = Color.Blue
+            btn5.BackColor = Color.Blue
+            btn7.BackColor = Color.Blue
+            MessageBox.Show("X is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn2.Text = "X" And btn5.Text = "X" And btn8.Text = "X" Then
+            btn2.BackColor = Color.Blue
+            btn5.BackColor = Color.Blue
+            btn8.BackColor = Color.Blue
+            MessageBox.Show("X is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+
+        If btn1.Text = "O" And btn2.Text = "O" And btn3.Text = "O" Then
+            btn1.BackColor = Color.Blue
+            btn2.BackColor = Color.Blue
+            btn3.BackColor = Color.Blue
+            MessageBox.Show("O is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn4.Text = "O" And btn5.Text = "O" And btn6.Text = "O" Then
+            btn4.BackColor = Color.Blue
+            btn5.BackColor = Color.Blue
+            btn6.BackColor = Color.Blue
+            MessageBox.Show("O is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn7.Text = "O" And btn8.Text = "O" And btn9.Text = "O" Then
+            btn7.BackColor = Color.Blue
+            btn8.BackColor = Color.Blue
+            btn9.BackColor = Color.Blue
+            MessageBox.Show("O is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn1.Text = "O" And btn4.Text = "O" And btn7.Text = "O" Then
+            btn1.BackColor = Color.Blue
+            btn4.BackColor = Color.Blue
+            btn7.BackColor = Color.Blue
+            MessageBox.Show("O is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn1.Text = "O" And btn5.Text = "O" And btn9.Text = "O" Then
+            btn1.BackColor = Color.Blue
+            btn5.BackColor = Color.Blue
+            btn9.BackColor = Color.Blue
+            MessageBox.Show("O is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn3.Text = "O" And btn6.Text = "O" And btn9.Text = "O" Then
+            btn3.BackColor = Color.Blue
+            btn6.BackColor = Color.Blue
+            btn9.BackColor = Color.Blue
+            MessageBox.Show("O is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn3.Text = "O" And btn5.Text = "O" And btn7.Text = "O" Then
+            btn3.BackColor = Color.Blue
+            btn5.BackColor = Color.Blue
+            btn7.BackColor = Color.Blue
+            MessageBox.Show("O is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+        If btn2.Text = "O" And btn5.Text = "O" And btn8.Text = "O" Then
+            btn2.BackColor = Color.Blue
+            btn5.BackColor = Color.Blue
+            btn8.BackColor = Color.Blue
+            MessageBox.Show("O is the Winner", "Good Game!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ButtonsEnableFalse()
+        End If
+
+    End Sub
+
+    Private Sub btn_click(sender As Object, e As EventArgs) Handles btn9.Click, btn8.Click, btn7.Click, btn6.Click, btn5.Click, btn4.Click, btn3.Click, btn2.Click, btn1.Click
+        Dim a As Button = sender
+        If XorO = False Then
+            a.Text = "X"
+            XorO = True
+        Else
+            a.Text = "O"
+            XorO = False
+        End If
+        check()
+        a.Enabled = False
+    End Sub
+End Class
